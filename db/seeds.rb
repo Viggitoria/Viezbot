4d83191380ab31508626f940a05010bf16146bcc
Viggitoria <s2viwebe@uni-trier.de>
2022-01-10 11:24:52 +0100

    updates von Freitag

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Question.create([
+                  { body: 'Wann wurde erstmalig ein Viezkönig gewählt?' },
+                  { body: 'Kennst du die "Viezstraße"? Wie lang geht die Viezstraßen-Saison?' },
+                  { body: 'Ist Viez bereits Teil des UNESCO Weltkulturerbes?' },
+                  { body: 'Aus welchen Schritten besteht die Viez Herstellung?' },
+                  { body: 'Wie entsteht Eis-Viez?' },
+                  { body: 'Was ist die Viezbruderschaft?' },
+                  { body: 'Wie wird Viez in der Moselregion traditionell serviert?' },
+                  { body: 'Wo wurde das Wort "Viez" zum ersten Mal verschriftlicht?' },
+                  { body: 'Bis zu wie viele verschiedene Tier- und Pflanzenarten beherbergen Streuobstwiesen?' },
+                  { body: 'Wo liegt auch heute noch das Zentrum des Streuobstanbaus zwischen Mosel und Saar?' }
+                ])
