0a88ae8e231c44cb20c7254805fa3e91c81d5ac2
Viggitoria <s2viwebe@uni-trier.de>
2022-01-16 16:30:32 +0100

    added javascript und css file

--- app/javascript/controllers/chatwindow_controller.js
+++ app/javascript/controllers/chatwindow_controller.js
@@ -1,7 +1,7 @@
-
-var answer_button = document.getElementsByClassName("answer_button");
+let answer_button;
+answer_button = document.getElementsByClassName("answer_button");
 Object.keys(answer_button).map( function (key) {
-    var button = answer_button[key]
+    let button = answer_button[key];
     button.addEventListener("click", function(){
         alert("Yay!")
     });
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'chatwindow#index'
   get '/chatwindow', to: 'chatwindow#index'
+  get '/chatwindow/next', to: 'chatwindow#next'
   # Defines the root path route ("/")
   # root "articles#index"
 end
