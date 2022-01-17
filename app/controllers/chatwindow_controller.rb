944426132f3af76097d0dfe77c445be40ea55b77
Viggitoria <s2viwebe@uni-trier.de>
2022-01-17 16:36:45 +0100

    changed controller and routes

--- app/controllers/chatwindow_controller.rb
+++ app/controllers/chatwindow_controller.rb
@@ -5,17 +5,18 @@ class ChatwindowController < ApplicationController
   def index
     @questions = Question.all
     @answers = Answer.all
-    @current_question = $questions.find(1)
-    @current_answer_a = $answers.find(1)
-    @current_answer_b = $answers.find(2)
-    @current_answer_c = $answers.find(3)
-
+    @current_question = @questions.find(1)
+    @current_answer_a = @answers.find(1)
+    @current_answer_b = @answers.find(2)
+    @current_answer_c = @answers.find(3)
   end
 
   def next
-    @current_question = $questions.find(params[:value].to_i + 1)
-    @current_answer_a = $answers.find(params[:value].to_i)
-    @current_answer_b = $answers.find(params[:value].to_i)
-    @current_answer_c = $answers.find(params[:value].to_i)
+    @questions = Question.all
+    @answers = Answer.all
+    @current_question = @questions.find(params[:value].to_i + 1)
+    @current_answer_a = @answers.find_by belongs_to: params[:value].to_i + 1
+    @current_answer_b = @answers.find_by belongs_to: params[:value].to_i + 1
+    @current_answer_c = @answers.find_by belongs_to: params[:value].to_i + 1
   end
 end
