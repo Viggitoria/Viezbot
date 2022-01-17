e2c12d6f6944e3c280ff8086cac3471ff580868d
Viggitoria <s2viwebe@uni-trier.de>
2022-01-17 20:15:43 +0100

    removed global variables

--- app/controllers/chatwindow_controller.rb
+++ app/controllers/chatwindow_controller.rb
@@ -15,8 +15,8 @@ class ChatwindowController < ApplicationController
     @questions = Question.all
     @answers = Answer.all
     @current_question = @questions.find(params[:value].to_i + 1)
-    @current_answer_a = @answers.find_by belongs_to: params[:value].to_i + 1
-    @current_answer_b = @answers.find_by belongs_to: params[:value].to_i + 1
-    @current_answer_c = @answers.find_by belongs_to: params[:value].to_i + 1
+    @current_answer_a = @answers.find(params[:value].to_i)
+    @current_answer_b = @answers.find(params[:value].to_i)
+    @current_answer_c = @answers.find(params[:value].to_i)
   end
 end
--- app/javascript/controllers/chatwindow_controller.js
+++
@@ -1,9 +0,0 @@
-let answer_button;
-answer_button = document.getElementsByClassName("answer_button");
-Object.keys(answer_button).map( function (key) {
-    let button = answer_button[key];
-    button.addEventListener("click", function(){
-        alert("Hello")
-
-    });
-});
