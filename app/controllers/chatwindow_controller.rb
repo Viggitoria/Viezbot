8de49b97853fd71e76a401311e2365ea95d94d54
Viggitoria <s2viwebe@uni-trier.de>
2022-01-18 08:13:13 +0100

    improved sql statements in controller index

--- app/controllers/chatwindow_controller.rb
+++ app/controllers/chatwindow_controller.rb
@@ -3,11 +3,11 @@ class ChatwindowController < ApplicationController
   layout 'application'
   def index
     @questions = Question.all
-    @answers = Answer.all
     @current_question = @questions.find(1)
-    @current_answer_a = @answers.find(1)
-    @current_answer_b = @answers.find(2)
-    @current_answer_c = @answers.find(3)
+    @answers = Answer.select(:id, :body).where(belongs_to: @current_question.id)
+    @current_answer_a = @answers.first
+    @current_answer_b = @answers.find(@current_answer_a.id + 1)
+    @current_answer_c = @answers.last
   end
 
   def next
