cf42e4fa84c1fa470dc8cc83046b4ce79900705d
Viggitoria <s2viwebe@uni-trier.de>
2022-01-20 10:40:09 +0100

    automatisation of code, removed next, changes seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'chatwindow#index'
   get '/chatwindow', to: 'chatwindow#index'
-  get '/next/:value', to: 'chatwindow#next'
-  get '/next', to: 'chatwindow#next'
+  get '/index/:value', to: 'chatwindow#index'
   # Defines the root path route ("/")
-  # root "articles#index"
 end
