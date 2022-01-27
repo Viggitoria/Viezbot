de5e0f28ff04d09ea8c5aadd8174d8da0fb59ad1
Viggitoria <s2viwebe@uni-trier.de>
2022-01-27 10:48:48 +0100

    randomized questions

--- app/controllers/chatwindow_controller.rb
+++ app/controllers/chatwindow_controller.rb
@@ -5,5 +5,13 @@ class ChatwindowController < ApplicationController
   def index
     @questions = Question.all
     @answers = Answer.all
+
+    @question_array = []
+
+    while @question_array.length < 10
+      rand_num = rand(1..10)
+      new_question = @questions.find(rand_num)
+      @question_array.append(new_question) if @question_array.exclude?(new_question)
+    end
   end
 end
