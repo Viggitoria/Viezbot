4650763f7e3245bd069885242e7e887c3b2f6a13
Viggitoria <s2viwebe@uni-trier.de>
2022-02-01 19:15:58 +0100

    changed indexes in controller

---
+++ db/migrate/20220129145337_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :answers do |t|
+      t.text :body
+      t.boolean :correct?
+      t.references :question, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
