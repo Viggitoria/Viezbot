c34e203ec3f0be37583eb4f96b75dff8252bcdca
Viggitoria <s2viwebe@uni-trier.de>
2022-02-06 20:20:56 +0100

    new data base input, fixed spelling

--- db/migrate/20220206191609_create_answers.rb
+++ db/migrate/20220206191609_create_answers.rb
@@ -1,5 +1,6 @@
 class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
+    create_table :answers do |t|
       t.text :body
       t.boolean :correct?
       t.references :question, null: false, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 
-#20 questions in total for the quiz
+# 20 questions in total for the quiz
 Question.create([
                   { body: 'Wann wurde erstmalig ein Viezkönig gewählt?' },
                   { body: 'Kennst Du die "Viezstraße"? Wie lang geht die Viezstraßen-Saison?' },
                   { body: 'Ist Viez bereits Teil des UNESCO Weltkulturerbes?' },
-                  { body: 'Aus welchen Schritten besteht die Viez Herstellung?' },
+                  { body: 'Aus welchen Schritten besteht die Viez-Herstellung?' },
                   { body: 'Was ist Eis-Viez?' },
                   { body: 'Was ist die Viezbruderschaft?' },
                   { body: 'Wie wird Viez in der Moselregion traditionell serviert?' },
@@ -19,6 +19,11 @@ Question.create([
                   { body: 'Woher kommt der Begriff Porz?' },
                   { body: 'Wie viel Kalorien enthalten 250ml Viez im Durchschnitt?' },
                   { body: 'Aus welchem Obst kann Viez hergestellt werden?' },
+                  { body: 'Wie wird Viez auf trierisch bezeichnet, wenn er besonders gut ist?' },
+                  { body: 'Am 18. April 2012 kam die Viezporz vor Gericht. Worum wurde gestritten?' },
+                  { body: 'Wie lang ist die Viezstraße?' },
+                  { body: 'Wann findet das Merziger Viezfest statt?' },
+                  { body: 'Welches dieser Lieder existiert wirklich?' }
                 ])
 
 Answer.create([
@@ -52,7 +57,7 @@ Answer.create([
                         'die erst nach dem ersten Frost geerntet wurden.', correct?: true, question_id: 5 },
                 { body: 'Viez mit besonders vielen Eiswürfeln.', correct?: false, question_id: 5 },
                 # Answers to question 6
-                { body: 'Ein im 15. Jhd. in Trier gegründeter christlicher Mönchsorden,' \
+                { body: 'Ein im 15. Jhd. in Trier gegründeter christlicher Mönchsorden, ' \
                         'der bis heute  Viez auf traditionelle Weise herstellt.', correct?: false, question_id: 6 },
                 { body: 'Ein in Trier ansässiger Vereien, der sich dem Erhalt und der Anerkennung ' \
                         'des Viez als Kulturgut  widmet.', correct?: true, question_id: 6 },
@@ -91,22 +96,69 @@ Answer.create([
                 # Answers to question 14
                 { body: '120-130', correct?: false, question_id: 14 },
                 { body: '70-90', correct?: false, question_id: 14 },
-                { body: '80-100', correct?: true , question_id: 14 },
+                { body: '80-100', correct?: true, question_id: 14 },
                 # Answers to question 15
-                { body: 'Äpfel und Pflaumen.', correct?: false , question_id: 15 },
-                { body: 'Äpfel und Quitten.', correct?: false , question_id: 15 },
-                { body: 'Äpfel und Birnen.', correct?: true , question_id: 15 }
+                { body: 'Äpfel und Pflaumen.', correct?: false, question_id: 15 },
+                { body: 'Äpfel und Quitten.', correct?: false, question_id: 15 },
+                { body: 'Äpfel und Birnen.', correct?: true, question_id: 15 },
+                # Answers to question 16
+                { body: 'HAMMER.', correct?: false, question_id: 16 },
+                { body: 'GAMMER.', correct?: false, question_id: 16 },
+                { body: 'SAUWER.', correct?: true, question_id: 16 },
+                # Answers to question 17
+                { body: 'Um die fehlende Füllmarkierung.', correct?: false, question_id: 17 },
+                { body: 'Um die Bezeichnung als Original.', correct?: true, question_id: 17 },
+                { body: 'Um Markenrechte.', correct?: false, question_id: 17 },
+                # Answers to question 18
+                { body: '175 km.', correct?: true, question_id: 18 },
+                { body: '325 km.', correct?: false, question_id: 18 },
+                { body: '200 km.', correct?: false, question_id: 18 },
+                # Answers to question 19
+                { body: 'Am ersten Samstag im Oktober.', correct?: true, question_id: 19 },
+                { body: 'Am letzten Sonntag im November.', correct?: false, question_id: 19 },
+                { body: 'Am ersten Samstag im November.', correct?: false, question_id: 19 },
+                # Answers to question 20
+                { body: 'Trier dein Viez.', correct?: false, question_id: 20 },
+                { body: 'Porzenlied.', correct?: false, question_id: 20 },
+                { body: 'Viez auf Hawaii.', correct?: true, question_id: 20 }
               ])
 
-#Fun Facts around Viez that are presented to the User randomly if he does not want play the quiz
+# Fun Facts around Viez that are presented to the User randomly if he does not want play the quiz
 FunFact.create([
                  { body: 'Wusstest Du, dass die gängige Abkürzung für Viez mit Cola Covie lautet?' },
-                 { body: 'Wusstest Du, dass sich unter den original Viezporzen der Aufdruck „GPS“ für Gebrüder Plein Speicher befindet?' },
-                 { body: 'Wusstest Du, dass in vielen Kneipen Viez gezapft wird? Es bedarf dafür jedoch eigens hergestellte Schläuche, da die Säure des Viez die handelsüblichen auflöst.' },
-                 { body: 'Wusstest Du, dass nicht überall der Viez aus einem Porz getrunken wird? Im Saarland reicht man das Getränk in einem Glaskrug.' },
+                 { body: 'Wusstest Du, dass sich unter den original Viezporzen der Aufdruck „GPS“ für Gebrüder '\
+                          'Plein Speicher befindet?' },
+                 { body: 'Wusstest Du, dass in vielen Kneipen Viez gezapft wird? Es bedarf dafür jedoch eigens '\
+                         'hergestellte Schläuche, da die Säure des Viez die handelsüblichen auflöst.' },
+                 { body: 'Wusstest Du, dass nicht überall der Viez aus einem Porz getrunken wird? Im Saarland reicht '\
+                          'man das Getränk in einem Glaskrug.' },
                  { body: 'Wusstest Du, dass man ca. 1,3kg Äpfel für einen Liter Viez benötigt?' },
-                 { body: 'Wusstest Du, dass es im Gegensatz zu vielen anderen traditionellen Getränken bei Viez keine Regeln gibt? Weder der Inhalt noch die Region in der das Getränk herstellt werden darf, ist begrenzt.' },
-                 { body: 'Wusstest Du, dass Viez erst in den letzten 40 Jahren wieder für viele Menschen rund um Trier attraktiv geworden ist? In der Mitte des letzten Jahrhunderts galt es als verstaubtes Altherrengetränk und fand bei jüngeren Menschen kaum Beachtung.'},
-                 { body: 'Wusstest Du, dass es in der Gemeinde Igel im Ortsteil Liersberg eine Viezallee gibt? Sie hat ihren Namen bekommen, weil der damals zuständige örtliche Revierleiter scherzhaft ein Schild mit "Viezallee" dort angebracht hat und dort früher zahlreiche Obstbäume standen.'},
-                 { body: 'Wusstest Du, dass es in Hagenow einen Stadtteil namens Viez gibt?'}
+                 { body: 'Wusstest Du, dass es im Gegensatz zu vielen anderen traditionellen Getränken bei Viez keine '\
+                         'Regeln gibt? Weder der Inhalt noch die Region in der das Getränk herstellt werden darf, ist '\
+                         'begrenzt.' },
+                 { body: 'Wusstest Du, dass Viez erst in den letzten 40 Jahren wieder für viele Menschen rund um Trier '\
+                         'attraktiv geworden ist? In der Mitte des letzten Jahrhunderts galt es als verstaubtes '\
+                         'Altherrengetränk und fand bei jüngeren Menschen kaum Beachtung.' },
+                 { body: 'Wusstest Du, dass es in der Gemeinde Igel im Ortsteil Liersberg eine Viezallee gibt? Sie hat '\
+                         'ihren Namen bekommen, weil der damals zuständige örtliche Revierleiter scherzhaft ein Schild '\
+                         'mit "Viezallee" dort angebracht hat und dort früher zahlreiche Obstbäume standen.' },
+                 { body: 'Wusstest Du, dass es in Hagenow einen Stadtteil namens Viez gibt? Woher sich der Name '\
+                         'ableitet, ist jedoch unbekannt. Es wird vermutet, dass der Name von der Weichselkirsche stammt.' },
+                 { body: 'Wusstest Du, dass es in Köln einen Stadtbezirk namens Porz gibt? Der Name des '\
+                         'rechtsrheinischen Stadtteils hat jedoch nichts mit dem Trinkgefäß zu tun.' },
+                 { body: 'Wusstest Du, dass man einen Hugo mit Viez auch Vigo nennt?' },
+                 { body: 'Wusstest Du, dass es DIE Porz heißt? Im Plural sind es die Porzen.' },
+                 { body: 'Wusstest Du, dass es eine jährliche Viez-Prämierung gibt? Seit über 20 Jahren vergibt das '\
+                         'Dienstleistungszentrum Ländlicher Raum (DLR) in Rheinland-Pfalz Zertifikate für Produkte '\
+                         'der Spitzenklasse. Die Viez-Produkte werden dafür nach Geruch, Farbe, Geschmack und '\
+                         'Harmonie beurteilt.' },
+                 { body: 'Wusstest Du, dass Viez nicht nur ein Naturprodukt, sondern auch vegan ist?' },
+                 { body: 'Wusstest Du, dass es einen Welt-Apfelwein-Tag gibt? Er findet jedes Jahr am 3. Juni statt.' },
+                 { body: 'Wusstest Du, dass die Porz nur aufgrund einer Ausnahmeregelung in der Gastronomie als '\
+                         'Trinkgefäß verwendet werden darf? Da sie innen keinen Füllstrich hat, verstößt sie '\
+                         'eigentlich gegen die europäische Schankgefäßverordnung.' },
+                 { body: 'Wusstest Du, dass Viez sich durchaus von anderen Apfelwein-Varianten wie Cider, Most oder '\
+                         'Ebbelwoi unterscheidet? Viez wird im Gegensatz zu einigen anderen Produkten vollständig '\
+                         'durchgegoren und nicht mit Kohlensäure versetzt. Spätestens in den verwendeten Apfelsorten '\
+                         'unterscheiden sich die Getränke.' }
                ])
