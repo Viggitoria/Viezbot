b9ab12cc740f18c45ff36f077101bd4afa0f48cd
Viktoria Weber <s2viwebe@uni-trier.de>
2022-02-20 15:06:19 +0100

    added helper function

--- app/helpers/chatwindow_helper.rb
+++ app/helpers/chatwindow_helper.rb
@@ -1,2 +1,9 @@
 module ChatwindowHelper
+
+  def load_random_fact
+    @new_num = rand(@fun_fact.first.id..@fun_fact.last.id)
+    @rand_fact = @fun_fact.find(@new_num)
+    @rand_fact.body
+
+  end
 end
